67978508e5487ca78129eaf7073eea94a8c9c0f7
davijvasconcellos-senai <davijvasconcellos@gmail.com>
2025-11-30 20:03:38 -0300

    Adição do esquema de banco de dados dos usuários

---
+++ squema.sql
@@ -0,0 +1,20 @@
+create database predictivepulse;
+use predictivepulse;
+
+create table usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    matricula CHAR(5) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
+    senha char(10) NOT NULL,
+    tipo char(15) NOT NULL
+);
+
+insert into usuarios values (
+    null, 
+    'Davi de Jesus Vasconcellos de Oliveira', 
+    'b-757', 
+    'adm_davi@email.com', 
+    'Senai*123', 
+    'administrador'
+);
